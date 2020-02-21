96b58fe54cc4f0d3f2571d2692c898e7d2e56ccd
VetalSh <60194255+VetalSh@users.noreply.github.com>
2020-02-21 09:01:54 +0200

    lesson3 + homework
    Fixed checkbox descriptions in homework.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const DAY_STRING = ['день', 'дня', 'дней'];
+
 const DATA = {
   whichSite: ['landing', 'multiPage', 'onlineStore'],
   price: [4000, 8000, 26000],
@@ -10,7 +12,7 @@ const DATA = {
   metrikaYandex: [500, 1000, 2000],
   analyticsGoogle: [850, 1350, 3000],
   sendOrder: 500,
-  deadlineDay: [[2, 7], [3, 7], [7, 14]],
+  deadlineDay: [[2, 7], [3, 10], [7, 14]],
   deadlinePercent: [20, 17, 15]
 };
 
@@ -21,21 +23,55 @@ const startButton = document.querySelector('.start-button'),
       endButton = document.querySelector('.end-button'),
       total = document.querySelector('.total'),
       fastRange = document.querySelector('.fast-range'),
-      totalPriceSum = document.querySelector('.total_price__sum');
+      totalPriceSum = document.querySelector('.total_price__sum'),
+      adapt = document.getElementById('adapt'),
+      mobileTemplates = document.getElementById('mobileTemplates'),
+      desktopTemplates = document.getElementById('desktopTemplates'),
+      editable = document.getElementById('editable'),
+      adaptValue = document.querySelector('.adapt_value'),
+      mobileTemplatesValue = document.querySelector('.mobileTemplates_value'),
+      desktopTemplatesValue = document.querySelector('.desktopTemplates_value'),
+      editableValue = document.querySelector('.editable_value'),
+      typeSite = document.querySelector('.type-site'),
+      maxDeadLine = document.querySelector('.max-deadline'),
+      rangeDeadline = document.querySelector('.range-deadline'),
+      deadlineValue = document.querySelector('.deadline-value');
 
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+                            0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
 
 function showElem(elem) {
   elem.style.display = 'block';
 }
 
 function hideElem(elem) {
-  elem.style.display = 'none'
+  elem.style.display = 'none';
+}
+
+function renderTextContent(total, site, maxDay, minDay) {
+
+  totalPriceSum.textContent = total;
+  typeSite.textContent = site;
+  maxDeadLine.textContent = declOfNum(maxDay, DAY_STRING);
+  rangeDeadline.min = minDay;
+  rangeDeadline.max = maxDay;
+  deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING);
+
+  adaptValue.textContent = adapt.checked ? 'Да' : 'Нет';
+  mobileTemplatesValue.textContent = mobileTemplates.checked ? 'Да' : 'Нет';
+  desktopTemplatesValue.textContent = desktopTemplates.checked ? 'Да' : 'Нет';
+  editableValue.textContent = editable.checked ? 'Да' : 'Нет';
 }
 
 function priceCalculation(elem) {
   let result = 0,
       index = 0,
-      options = [];
+      options = [],
+      site = '',
+      maxDeadlineDay = DATA.deadlineDay[index][1],
+      minDeadlineDay = DATA.deadlineDay[index][0];
 
   if (elem.name === 'whichSite') {
     for (const item of formCalculate.elements) {
@@ -49,6 +85,9 @@ function priceCalculation(elem) {
   for (const item of formCalculate.elements) {
     if (item.name === 'whichSite' && item.checked) {
       index = DATA.whichSite.indexOf(item.value);
+      site = item.dataset.site;
+      maxDeadlineDay = DATA.deadlineDay[index][1];
+      minDeadlineDay = DATA.deadlineDay[index][0];
     } else if (item.classList.contains('calc-handler') && item.checked) {
       options.push(item.value)
     }
@@ -72,11 +111,20 @@ function priceCalculation(elem) {
 
   result += DATA.price[index];
 
-  totalPriceSum.textContent = result;
+  renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
+
+  
 }
 
-function handlerCallBackForm() {
+function handlerCallBackForm(event) {
   const target = event.target;
+
+  if (adapt.checked) {
+    mobileTemplates.disabled = false;
+  } else {
+    mobileTemplates.disabled = true;
+    mobileTemplates.checked = false;
+  }
   
   if (target.classList.contains('want-faster')) {
     target.checked ? showElem(fastRange) : hideElem(fastRange);
